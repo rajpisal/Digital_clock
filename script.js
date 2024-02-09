53fd38b05a24b69c0d4d2532b829761f546b2021
rajpisal <159114251+rajpisal@users.noreply.github.com>
2024-02-09 23:11:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+  
+    // Ensure two-digit format
+    hours = (hours < 10) ? '0' + hours : hours;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+  
+    var timeString = hours + ':' + minutes + ':' + seconds;
+  
+    document.getElementById('clock').innerText = timeString;
+  }
+  
+  
+  setInterval(updateClock, 1000);
+  
+  
+  updateClock();
